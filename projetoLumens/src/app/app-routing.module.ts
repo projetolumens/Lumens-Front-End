813c6d11283cc1e0abfe3049567116da50709673
unknown <coelhaoyagoo@gmail.com>
2022-02-21 15:01:36 -0300

    Cadastro

--- projetoLumens/src/app/app-routing.module.ts
+++ projetoLumens/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', redirectTo: 'entrar', pathMatch: 'full'},
+
+  {path:'cadastrar', component: CadastrarComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
