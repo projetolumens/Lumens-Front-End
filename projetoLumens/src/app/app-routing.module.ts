c7b9a1c5469be2f51e9dbc4e4321ed8ef28b285c
Giuliana <giulianatamie@gmail.com>
2022-03-01 20:32:50 -0300

    Criado componente contato

--- projetoLumens/src/app/app-routing.module.ts
+++ projetoLumens/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { ContatoComponent } from './contato/contato.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 
@@ -8,7 +9,8 @@ const routes: Routes = [
   {path:'', redirectTo: 'entrar', pathMatch: 'full'},
   {path:'entrar', component: EntrarComponent},
   {path:'cadastrar', component: CadastrarComponent},
-  {path:'inicio', component:InicioComponent}
+  {path:'inicio', component:InicioComponent},
+  {path:'contato', component: ContatoComponent}
 
 ];
 
