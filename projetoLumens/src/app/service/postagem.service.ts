93a2c46618cb200e7ee5f0346697baa10f2afdd3
Erika Yamamoto <erika.s.yamamoto@gmail.com>
2022-03-07 17:52:48 -0300

    Poategm para postagens

--- projetoLumens/src/app/service/postagem.service.ts
+++ projetoLumens/src/app/service/postagem.service.ts
@@ -19,23 +19,23 @@ export class PostagemService {
   } /* httpheaders : Para inserir o token no Authorization e no Header da minha requisição.*/
 
   getAllPostagens(): Observable<Postagem[]> {
-    return this.http.get<Postagem[]>('http://localhost:8080/postagem', this.token)
+    return this.http.get<Postagem[]>('http://localhost:8080/postagens', this.token)
   }
   /*Observable nos metódos que irão chamar os end-points Para garantir que o tipo da variável será passado corretamente.*/
 
   getByIdPostagem(id: number): Observable<Postagem> {
-    return this.http.get<Postagem>(`http://localhost:8080/postagem/${id}`, this.token)
+    return this.http.get<Postagem>(`http://localhost:8080/postagens/${id}`, this.token)
   }
 
   postPostagem(postagem: Postagem): Observable<Postagem> {
-    return this.http.post<Postagem>('http://localhost:8080/postagem', postagem, this.token)
+    return this.http.post<Postagem>('http://localhost:8080/postagens', postagem, this.token)
   }
 
   putPostagem(postagem: Postagem): Observable<Postagem> {
-    return this.http.put<Postagem>('http://localhost:8080/postagem', postagem, this.token)
+    return this.http.put<Postagem>('http://localhost:8080/postagens', postagem, this.token)
   }
 
   deletePostagem(id: number) {
-    return this.http.delete(`http://localhost:8080/postagem/${id}`, this.token)
+    return this.http.delete(`http://localhost:8080/postagens/${id}`, this.token)
   }
 }
